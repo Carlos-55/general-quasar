621706e5c2ce948791077d9e3fd1cfc00d681f8c
Gael <gsolis@e-bitware.com.mx>
2023-10-26 17:04:10 -0500

    Modificaiones en recursividad y adecuaciones en la vista de escritorio

--- src/pages/views-app/Publications.vue
+++ src/pages/views-app/Publications.vue
@@ -1,65 +1,92 @@
 <template>
-    
-    <div class="col-md-6 col-12">
-      <div v-if="articleFlag">
-        <ShowPublication @onClose="closeFlag"></ShowPublication>
-      </div>
-      <div v-if="publications">
+  <div class="col-md-6 col-12">
+    <div v-if="articleFlag">
+      <ShowPublication @onClose="closeFlag"></ShowPublication>
+    </div>
+    <div v-if="publications">
+      <div v-if="$q.screen.width > 900">
 
+        <div id="q-app" style="min-height: 100vh;">
+
+          <div class="q-pa-md row items-start q-gutter-md">
+            <div v-for="item in gael">
+              <q-card class="my-card" clickable v-ripple @click="showArticle()">
+                <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+                <q-card-section>
+                  <div class="text-h6">Titulo de la publicacion</div>
+                  <div class="text-subtitle2">25/05/2023</div>
+                </q-card-section>
+
+                <q-card-section class="q-pt-none">
+                  {{ 'CONTENIDO DE LA PUBLICACION Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' }}
+                </q-card-section>
+              </q-card>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div v-if="$q.screen.width < 900">
         <div v-for="item in gael">
           <q-item clickable v-ripple @click="showArticle()">
-          <q-item-section avatar>
-            <q-avatar rounded>
-              <img src="https://cdn.quasar.dev/img/mountains.jpg">
-            </q-avatar>
-          </q-item-section>
-          <div class="">
+            <q-item-section avatar>
+              <q-avatar rounded>
+                <img src="https://cdn.quasar.dev/img/mountains.jpg">
+              </q-avatar>
+            </q-item-section>
+            <div class="">
               <q-item-section></q-item-section>
-              <q-item-section class="text-subtitle2 text-weight-bold">Titulo de la publicaion</q-item-section>
+              <q-item-section class="text-subtitle2 text-weight-bold">Titulo de la publicacion</q-item-section>
               <q-item-section>Espacio</q-item-section>
               <q-item-section>26/sep/2023</q-item-section>
-          </div>
-        </q-item>  
-          </div>
+            </div>
+          </q-item>
+        </div>
       </div>
-      
+
     </div>
-  
-  </template>
-  <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
-  import ShowPublication from '../views-app/Show.vue'
-  //@ts-ignore
-  @Component({
-    components : {
-      ShowPublication,
-    }
-  }) 
-  export default class ViewPublications extends Vue {
-    gael: Array<any> = ["asddf","gae","mart","das","HTML", "CSS"];
-      articleFlag :boolean = false
-      publications : boolean = true
-    
-    
-    async mounted() {
-      
-    }
 
-    showArticle(){
-        this.publications = false
-        this.articleFlag= !this.articleFlag
-    }
-
-    closeFlag(closeShow: boolean){
-      this.publications = true
-      this.articleFlag= closeShow
-    }
-
-        
-    
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import ShowPublication from '../views-app/Show.vue'
+//@ts-ignore
+@Component({
+  components: {
+    ShowPublication,
   }
-  </script>
-  <style>
-  
-  </style>
+})
+export default class ViewPublications extends Vue {
+  gael: Array<any> = ["asddf", "gae", "mart", "das", "HTML", "CSS"];
+  articleFlag: boolean = false
+  publications: boolean = true
+
+
+
+  async mounted() {
+
+  }
+
+  showArticle() {
+    this.publications = false
+    this.articleFlag = !this.articleFlag
+  }
+
+  closeFlag(closeShow: boolean) {
+    this.publications = true
+    this.articleFlag = closeShow
+  }
+
+
+
+}
+</script>
+<style lang="sass" scoped>
+.my-card
+  width: 100%
+  max-width: 250px
+</style>
+
   
--- src/pages/views-app/Raiting.vue
+++ src/pages/views-app/Raiting.vue
@@ -1,31 +1,53 @@
 <template>
-    
-    <div class="col-md-6 col-12">
-        <div class="q-ma-md">Consulta la boleta de:
-     
+<div class="col-md-6 col-12">
+    <div class="q-ma-md text-h6">Consulta la boleta de:
+
+    </div>
+    <div v-if="$q.screen.width > 900">
+
+        <div id="q-app" style="min-height: 100vh;">
+
+            <div class="q-pa-md row items-start q-gutter-md">
+                <div v-for="item in gael">
+                    <q-card class="my-card" clickable>
+                        <img src="https://cdn.quasar.dev/img/mountains.jpg">
+
+                        <q-card-section>
+                            <div class="text-h6">Pedro Juaquin Gonzales Hernandez</div>
+                            <div class="text-subtitle2">25/05/2023</div>
+                        </q-card-section>
+                    </q-card>
+                </div>
+
+            </div>
         </div>
-    
+    </div>
+
+    <div v-if="$q.screen.width < 900">
         <q-item clickable v-ripple>
-        <q-item-section class="q-mr-xs" avatar>
-          <q-avatar rounded size="70px">
-            <img src="https://cdn.quasar.dev/img/mountains.jpg">
-          </q-avatar>
-        </q-item-section>
-        <div>
-            <q-item-section class="text-h6 text-weight-bold " >Pedro juaquin de la cueva Hernández </q-item-section>
-        </div>
-      </q-item>
-      <q-item clickable v-ripple>
-        <q-item-section class="q-mr-xs" avatar>
-          <q-avatar rounded size="70px">
-            <img src="https://cdn.quasar.dev/img/mountains.jpg">
-          </q-avatar>
-        </q-item-section>
-        <div>
-            <q-item-section class="text-h6 text-weight-bold " >Pedro juaquin de la cueva Hernández </q-item-section>
-        </div>
-      </q-item> 
-      <!-- <div v-for="item in gael">
+            <q-item-section class="q-mr-xs" avatar>
+                <q-avatar rounded size="70px">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                </q-avatar>
+            </q-item-section>
+            <div>
+                <q-item-section class="text-h6 text-weight-bold ">Pedro juaquin de la cueva Hernández </q-item-section>
+            </div>
+        </q-item>
+        <q-item clickable v-ripple>
+            <q-item-section class="q-mr-xs" avatar>
+                <q-avatar rounded size="70px">
+                    <img src="https://cdn.quasar.dev/img/mountains.jpg">
+                </q-avatar>
+            </q-item-section>
+            <div>
+                <q-item-section class="text-h6 text-weight-bold ">Pedro juaquin de la cueva Hernández </q-item-section>
+            </div>
+        </q-item>
+
+    </div>
+
+    <!-- <div v-for="item in gael">
         <q-item clickable v-ripple>
         <q-item-section avatar>
           <q-avatar rounded>
@@ -41,29 +63,34 @@
       </q-item>  
         </div>
        -->
-    </div>
-  
-  </template>
-  <script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
+</div>
+</template>
 
-  //@ts-ignore
-  @Component({
-    components : {}
-  }) 
-  export default class ViewRaiting extends Vue {
-    gael: Array<any> = ["asddf","gae","mart","das","HTML", "CSS"];
-    
-    
+  
+<script lang="ts">
+import {
+    Component,
+    Prop,
+    Vue
+} from 'vue-property-decorator';
+
+//@ts-ignore
+@Component({
+    components: {}
+})
+export default class ViewRaiting extends Vue {
+    gael: Array < any > = ["asddf", "gae", "mart", "das", "HTML", "CSS"];
+
     async mounted() {
-      ;
-      
+        ;
+
     }
-        
-    
-  }
-  </script>
-  <style>
+
+}
+</script>
   
-  </style>
-  
+<style lang="sass" scoped>
+.my-card 
+  width: 100% 
+  max-width: 250px
+</style>
